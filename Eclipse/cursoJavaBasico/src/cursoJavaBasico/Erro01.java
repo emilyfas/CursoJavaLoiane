c49f5c6af1a4a9fefba36ef3bb510ce9ec3813fc
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-05-11 10:46:26 -0300

    adcionando eclipse

---
+++ Eclipse/cursoJavaBasico/src/cursoJavaBasico/Erro01.java
@@ -0,0 +1,11 @@
+package cursoJavaBasico;
+
+public class Erro01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World Erro 01");
+		
+	}
+
+}
---
+++ Eclipse/cursoJavaBasico/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Renata
+ *
+ */
+module cursoJavaBasico {
+}
